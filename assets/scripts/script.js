e353f1cc3c782eca4b30d2752672b1a1ef6fe368
Ayman Cabotaje <ayman@drtalks.com>
2023-10-25 20:51:56 +0800

    change in index, style and script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,1 @@
-const body = document.body
-const div = document.querySelector('div')
-const spanHi = document.querySelector('#hi')
-const spanBye = document.querySelector('#bye')
-
-spanHi.style.color = "red"
-spanHi.style.backgroundColor = "blue"
+console.log("Hello World")
